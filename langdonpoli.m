8495ac5b01c24318f504627bc18196eb22fd1dda
Andres Munoz Acosta <munoz.m@unimelb.edu.au>
2020-03-11 13:43:10 +1100

    Update langdonpoli.m

--- langdonpoli.m
+++ langdonpoli.m
@@ -1,17 +1,21 @@
-function Y = landgonpoli(X,fid)
+function Y = langdonpoli(X,fid)
 % -------------------------------------------------------------------------
-% munozsmithmiles.m
+% langdonpoli.m
 % -------------------------------------------------------------------------
 %
 % Functions from Landgon and Poli 2007 (IEEE Tran. Evol. Comput.) generated
 % for the paper "Generating New Space-Filling Test Instances for Continuous
-% Black-Box Optimization" Accepted in Evol. Comput. 2019
+% Black-Box Optimization" Accepted in Evol. Comput. 2019.
+% 
+% To make these functions' range, which is [-10 10]^2, compatible with the
+% range defined in the BBOB benchmark set, which is [-5 5]^2, X is
+% multiplied by 2, and Y is made zero if X exceeds the bounds.
 %
 % By: Mario Andres Munoz Acosta
 %     School of Mathematics and Statistics
 %     The University of Melbourne
 %     Australia
-%     2019
+%     2020
 %
 % Input: 
 %   X       - a matrix of (d x N) candidate solutions.
@@ -21,7 +25,9 @@ function Y = landgonpoli(X,fid)
 % Output:
 %   Y       - a (N) vector of fitness values
 %
- 
+
+X = 2.*X;
+
 evalstr = {@(x) 0.11 + 0.77.*x(:,1).*(1 - x(:,1)) - 0.075.*x(:,2);          % Fig 2
            @(x) 0.127 + 0.063.*x(:,1);                                      % Fig 3
            @(x) x(:,2).*(1.32 + 1.78.*x(:,1) - x(:,1).^2 - x(:,2)) + 0.37;  % Fig 4
@@ -29,10 +35,10 @@ evalstr = {@(x) 0.11 + 0.77.*x(:,1).*(1 - x(:,1)) - 0.075.*x(:,2);          % Fi
            @(x) 0.063.*x(:,1) - 0.052;                                      % Fig 6
            @(x) -(0.171 + 0.0188.*x(:,2)).*x(:,2);                          % Fig 7
            @(x) 0.00124.*(x(:,1).^2).*x(:,2);                               % Fig 12
-           @(x) x(:,1).*(0.643 + x(:,2) - x(:,1).*(0.299.*x(:,1) + 2.81 + x(:,2) + x(:,2).^2));
+           @(x) x(:,1).*(0.643 + x(:,2) - x(:,1).*(0.299.*x(:,1) + 2.81 + x(:,2) + x(:,2).^2)); % Fig 13
            @(x) (1.27 - 1.1.*x(:,1) - 0.53.*x(:,1).^2).*x(:,1);             % Fig 20
            @(x) (0.33 - 0.32.*x(:,1) - 2.32.*x(:,2)).*x(:,2);               % Fig 21
-           @(x) x(:,2).*(0.093 + 0.39.*x(:,2) + 0.15.*x(:,2).^2 - 0.17.*x(:,2).^3 - (0.19.*x(:,2).^2 + 0.20.*x(:,2).^3).*x(:,1).^2);
+           @(x) x(:,2).*(0.093 + 0.39.*x(:,2) + 0.15.*x(:,2).^2 - 0.17.*x(:,2).^3 - (0.19.*x(:,2).^2 + 0.20.*x(:,2).^3).*x(:,1).^2); % Fig 22
            @(x) x(:,1) - (x(:,1) - 1)./x(:,1);                              % Fig 23
            @(x) 0.063.*x(:,1);                                              % Fig 24
            @(x) -(0.13 + 0.24.*x(:,2)).*x(:,2);                             % Fig 25
@@ -48,5 +54,6 @@ if fid>length(evalstr)
 end
 
 Y = feval(evalstr{fid},X');
+Y(any(abs(X)>10,1)) = 0;
 
 end
